7b6579eb6dc8fe9237089b36173db7df938c9639
Nhữ Văn Hữu <1910127@dlu.edu.vn>
2022-09-08 15:20:17 +0700

    huu finish work at the first time

--- Web/Pages/RoutePage.aspx.cs
+++ Web/Pages/RoutePage.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessLayer.DBAccess;
 
 namespace Web.Pages
 {
@@ -11,7 +12,73 @@ namespace Web.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                RoutesNumber.Text = "Hello";
+            }
+        }
 
+        protected void ButtonAddRoute_Click(object sender, EventArgs e)
+        {
+            onTestGetData();
+        }
+        private void onTestGetData()
+        {
+            Route route = GetRoute();
+
+            this.TestPlace.Text = $"ID: {route.RouteID} \n" +
+                $"IDBusRoute: {route.BusRoutesID}\n" +
+                $"IDStartPoint: {route.StartPositionID} \n" +
+                $"IDEndPoint: {route.EndPositionID}\n" +
+                $"RouteName: {route.RouteName}\n" +
+                $"RouteAmount: {route.RouteAmount}\n" +
+                $"RouteTime: {route.RouteTime}\n" +
+                $"RouteStartActiveTime: {route.StartTime}\n" +
+                $"RouteFinishActiveTime: {route.EndTime}\n" +
+                $"Applicable: {route.ApplicableDate}\n" +
+                $"Operation: {route.OperationDate}";
+        }
+        private Route GetRoute()
+        {
+            Route route = new Route();
+            int IDRoute = -1;
+            int IDBusRoutes = -1;
+            int IDStartPoint = -1;
+            int IDEndPoint = -1;
+            string RouteName;
+            int RouteAmount = 0;
+            double _doubleRouteTime = 0;
+            TimeSpan RouteTime = TimeSpan.Zero;
+
+            int.TryParse(this.RouteIDRoute.Text, out IDRoute);
+            int.TryParse(this.RouteIDBusRoute.Text, out IDBusRoutes);
+            int.TryParse(this.RouteIDStartPoint.Text, out IDStartPoint);
+            int.TryParse(this.RouteIDEndPoint.Text, out IDEndPoint);
+            RouteName = this.RouteRouteName.Text;
+            int.TryParse(this.RoutesNumber.Text, out RouteAmount);
+            //TimeSpan.TryParse(this.RoutesTime.Text, out RouteTime);
+            if (Double.TryParse(this.RoutesTime.Text, out _doubleRouteTime))
+            {
+                RouteTime = TimeSpan.FromMinutes(_doubleRouteTime);
+            }
+            DateTime RouteStartActiveTime = DateTime.Parse(this.RouteStartActiveTime.Text);
+            DateTime RouteFinishActiveTime = DateTime.Parse(this.RouteFinishActiveTime.Text);
+
+            DateTime RouteApplicable = DateTime.Parse(this.RouteApplicable.Text);
+            DateTime RouteOperation = DateTime.Now;
+
+            route.RouteID = IDRoute;
+            route.BusRoutesID = IDBusRoutes;
+            route.StartPositionID = IDStartPoint;
+            route.EndPositionID = IDEndPoint;
+            route.RouteName = RouteName;
+            route.RouteAmount = RouteAmount;
+            route.RouteTime = RouteTime;
+            route.StartTime = RouteStartActiveTime;
+            route.EndTime = RouteFinishActiveTime;
+            route.ApplicableDate = RouteApplicable;
+            route.OperationDate = RouteOperation;
+            return route;
         }
     }
 }
--- Web/Pages/StopRoutePage.aspx.cs
+++ Web/Pages/StopRoutePage.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessLayer.DBAccess;
 
 namespace Web.Pages
 {
@@ -12,6 +13,40 @@ namespace Web.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void ButtonAddStopRoute_Click(object sender, EventArgs e)
+        {
+            onTestGetStopRoute();
+        }
+        private void onTestGetStopRoute()
+        {
+            Stop_Route sr = GetStopRoute();
+            TestPlace.Text = $"ID: {sr.StopRouteID}\n" +
+                $"IDRoute: {sr.RouteID}\n" +
+                $"IDEndPosition: {sr.EndPositionID}\n" +
+                $"Order: {sr.Order}";
+
+        }
+        private Stop_Route GetStopRoute()
+        {
+            Stop_Route stopRoute = new Stop_Route();
+            int StopRouteID = 0;
+            int RouteID=0;
+            int EndPositionID=0;
+            int Orther= 0;
+               
+            int.TryParse(this.StopRouteIDStopRoute.Text, out StopRouteID);
+            int.TryParse(this.StopRouteIDRoute.Text, out RouteID);
+            int.TryParse(this.StopRouteIDStoping.Text, out EndPositionID);
+            int.TryParse(this.StopRouteOrder.Text, out Orther);
+
+            stopRoute.StopRouteID = StopRouteID;
+            stopRoute.RouteID = RouteID;
+            stopRoute.EndPositionID = EndPositionID;
+            stopRoute.Order = Orther;
+
+            return stopRoute;
         }
     }
 }
